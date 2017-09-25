8b0342cd7f06193c25219e8d892e8b1572b8a4fb
klight1994 <gmarble94@gmail.com>
2017-09-25 20:03:34 +0900

    User Class 단순화

--- app/src/main/java/crossfit_juan/chk/com/crossfitjuan/Common/User.java
+++ app/src/main/java/crossfit_juan/chk/com/crossfitjuan/Common/User.java
@@ -15,13 +15,8 @@ public class User {
     public static User getInstance(){
         return Me;
     }
-    public void setUser(UserData a){
-        data=new UserData();
-        data.setUser_access_key(a.getUser_access_key());
-        data.setUser_email(a.getUser_email());
-        data.setUser_gender(a.getUser_gender());
-        data.setUser_name(a.getUser_name());
-        data.setUser_phone_number(a.getUser_phone_number());
+    public void setUser(UserData data){
+        this.data=data;
     }
 
     public UserData getData() {
