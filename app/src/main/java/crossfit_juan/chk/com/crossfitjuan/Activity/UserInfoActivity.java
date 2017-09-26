0ee55128a755f5c7a241ba3f8f891d0213742e4a
gyuhwan <gmarble94@gmail.com>
2017-09-26 18:52:18 +0900

    프로필 이미지 변경 기능 추가

--- app/src/main/java/crossfit_juan/chk/com/crossfitjuan/Activity/UserInfoActivity.java
+++ app/src/main/java/crossfit_juan/chk/com/crossfitjuan/Activity/UserInfoActivity.java
@@ -1,18 +1,28 @@
 package crossfit_juan.chk.com.crossfitjuan.Activity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import crossfit_juan.chk.com.crossfitjuan.Common.User;
 import crossfit_juan.chk.com.crossfitjuan.R;
+import crossfit_juan.chk.com.crossfitjuan.tool.AWSService;
+
+import static crossfit_juan.chk.com.crossfitjuan.Common.Constants.PICK_FROM_ALBUM_ACTION;
 
 public class UserInfoActivity extends AppCompatActivity {
 
@@ -41,6 +51,9 @@ public class UserInfoActivity extends AppCompatActivity {
     @BindView(R.id.button)
     Button button;
 
+    private final String TAG="DEBUG";
+    Uri ImageCaptureUrl=null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,11 +68,41 @@ public class UserInfoActivity extends AppCompatActivity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.imageButton:
-                Toast.makeText(getApplicationContext(),"이미지 변경 버튼",Toast.LENGTH_LONG).show();
+//                Toast.makeText(getApplicationContext(),"이미지 변경 버튼",Toast.LENGTH_LONG).show();
+                TakeImagetoAlbum();
                 break;
             case R.id.button:
                 Toast.makeText(getApplicationContext(),"기간 변경 버튼",Toast.LENGTH_LONG).show();
                 break;
         }
     }
+    public void TakeImagetoAlbum(){
+        Intent intent=new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent,PICK_FROM_ALBUM_ACTION);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode,int resultCode,Intent data){
+        if(resultCode!=RESULT_OK){
+            return;
+        }
+        switch (requestCode){
+            case PICK_FROM_ALBUM_ACTION :
+                ImageCaptureUrl=data.getData();
+                String realpath=getImagePath(ImageCaptureUrl);
+                 AWSService.getInstance().uploadFileThread(new File(realpath));
+                break;
+        }
+    }
+    public String getImagePath(Uri uri) {
+        String[] projection = {MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        startManagingCursor(cursor);
+        int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(columnIndex);
+    }
+
 }
