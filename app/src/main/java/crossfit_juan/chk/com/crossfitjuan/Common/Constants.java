8d384fcdc14d8118cc8f37b74a73398f202da3eb
gyuhwan <gmarble94@gmail.com>
2017-09-30 16:20:21 +0900

    유저 삭제 URL 추가

--- app/src/main/java/crossfit_juan/chk/com/crossfitjuan/Common/Constants.java
+++ app/src/main/java/crossfit_juan/chk/com/crossfitjuan/Common/Constants.java
@@ -22,7 +22,7 @@ public class Constants {
     public static final String REQ_REGISTER_RESERVATION="http://52.78.197.131:25478/res_enroll_classes_m";
     public static final String REQ_GET_NOTICE="http://52.78.197.131:25478/read_notification/list";
     public static final String REQ_GET_NOTICE_CONTENT="http://52.78.197.131:25478/read_notification/item";
-
+    public static final String REQ_DELETE_USER="http://52.78.197.131:25478/delete_user";
     public static final String MAKE_ROOM_URL = "http://52.78.197.131:25478/sync_message_log";
 
     public static final int PICK_FROM_ALBUM_ACTION=0x80;
