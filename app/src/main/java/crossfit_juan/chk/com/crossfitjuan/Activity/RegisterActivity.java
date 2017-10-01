5dc8fcf5be93f65f9030f31dd43d96bb52c91120
gyuhwan <gmarble94@gmail.com>
2017-10-01 20:41:28 +0900

    레이아웃 수정

--- app/src/main/java/crossfit_juan/chk/com/crossfitjuan/Activity/RegisterActivity.java
+++ app/src/main/java/crossfit_juan/chk/com/crossfitjuan/Activity/RegisterActivity.java
@@ -5,8 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,6 +15,9 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import crossfit_juan.chk.com.crossfitjuan.Common.Constants;
 import crossfit_juan.chk.com.crossfitjuan.Common.User;
 import crossfit_juan.chk.com.crossfitjuan.DataModel.UserData;
@@ -26,6 +30,23 @@ import crossfit_juan.chk.com.crossfitjuan.R;
 
 public class RegisterActivity extends Activity {
 
+    @BindView(R.id.register_name)
+    EditText registerName;
+    @BindView(R.id.register_birthday_year)
+    EditText registerBirthdayYear;
+    @BindView(R.id.register_birthday_month)
+    EditText registerBirthdayMonth;
+    @BindView(R.id.register_birthday_day)
+    EditText registerBirthdayDay;
+    @BindView(R.id.register_phone_00)
+    EditText registerPhone00;
+    @BindView(R.id.register_phone_01)
+    EditText registerPhone01;
+    @BindView(R.id.register_phone_02)
+    EditText registerPhone02;
+    @BindView(R.id.req_registerBtn)
+    Button reqRegisterBtn;
+
     // 회원 등록화면
     // user_data : 사용자의 정보를 저장하는 객체
 
@@ -35,20 +56,13 @@ public class RegisterActivity extends Activity {
 
     private UserData user_data;
 
-    private EditText edt_yyyy;
-    private EditText edt_mm;
-    private EditText edt_dd;
-    private EditText[] edt_phone_number;
-
-    private EditText edt_name;
-    private Button btn_req_register;
-
     private Context my_context;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
 
         my_context = this;
         user_data = new UserData();
@@ -56,93 +70,109 @@ public class RegisterActivity extends Activity {
         Intent intent = getIntent();
         user_data.setUser_access_key(intent.getExtras().getString("access_key"));
         user_data.setUser_email(intent.getExtras().getString("id_email"));
-     //   user_data.setUser_name(intent.getExtras().getString("name"));
+        //   user_data.setUser_name(intent.getExtras().getString("name"));
         user_data.setUser_gender(intent.getExtras().getString("gender"));
+        makeEditTextListenersrcTodst(registerBirthdayYear,4,registerBirthdayMonth);
+        makeEditTextListenersrcTodst(registerBirthdayMonth,2,registerBirthdayDay);
+        makeEditTextListenersrcTodst(registerBirthdayDay,2,registerPhone00);
+        makeEditTextListenersrcTodst(registerPhone00,3,registerPhone01);
+        makeEditTextListenersrcTodst(registerPhone01,4,registerPhone02);
 
-        edt_yyyy = (EditText)findViewById(R.id.edttxt_register_yyyy);
-        edt_mm = (EditText)findViewById(R.id.edttxt_register_mm);
-        edt_dd = (EditText)findViewById(R.id.edttxt_register_mm);
-        edt_name= (EditText)findViewById(R.id.edttxt_register_name);
-        edt_phone_number=new EditText[3];
-        edt_phone_number[0]= (EditText)findViewById(R.id.edttxt_register_phonenumber_00);
-        edt_phone_number[1]= (EditText)findViewById(R.id.edttxt_register_phonenumber_01);
-        edt_phone_number[2]= (EditText)findViewById(R.id.edttxt_register_phonenumber_02);
+    }
 
-        btn_req_register = (Button)findViewById(R.id.btn_register_req_register);
-        btn_req_register.setOnClickListener(new View.OnClickListener() {
+    void makeEditTextListenersrcTodst(final EditText src, final int length, final EditText dst){
+        src.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                String birthday = edt_yyyy.getText().toString() + edt_mm .getText().toString() + edt_dd.getText().toString();
-                boolean isCorrectPhone=true;
-                for(int i=0;i<3;i++){
-                    if(edt_phone_number[i].getText().toString().equals("")){
-                        isCorrectPhone=false;
-                    }
-                }
-                String phone_number = edt_phone_number[0].getText().toString()+"-"+edt_phone_number[1].getText().toString()+"-"+edt_phone_number[2].getText().toString();
-                String name = edt_name.getText().toString();
-                if(!birthday.equals("") && isCorrectPhone && !name.equals("")) {
-                    JSONObject send_data = new JSONObject();
-                    try {
-                        user_data.setUser_name(name);
-                        send_data.put("access_key", user_data.getUser_access_key());
-                        send_data.put("gender", user_data.getUser_gender());
-                        send_data.put("birthday", birthday);
-                        send_data.put("phone_number", phone_number);
-                        send_data.put("name", user_data.getUser_name());
-                    } catch (JSONException jsonex) {
-                        jsonex.printStackTrace();
-                    }
-                    Log.i("name data", user_data.getUser_name());
-                    ReqHTTPJSONThread thread = new ReqHTTPJSONThread(Constants.REGISTER_MEMBER_URL, send_data);
-                    thread.start();
-                    try {
-                        thread.join();
-                    } catch (InterruptedException interex) {
-                        interex.printStackTrace();
-                    }
-                    String result = thread.handler.getMsg();
-                    JSONObject result_data = null;
-                    boolean flag = false;
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-                    try {
-                        result_data = new JSONObject(result);
-                        if (result_data.getInt("code") == 1110) {
-                            flag = true;
-                        }
-                    } catch (JSONException jsonex) {
-                        jsonex.printStackTrace();
-                    }
+            }
 
-                    if (flag) {
-                        Toast.makeText(my_context, "JUAN CROSS-FIT 가입을 환영합니다..", Toast.LENGTH_LONG);
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.length()==length){dst.requestFocus();}
+            }
 
-                        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-
-                        // 메인 액티비티로 넘길 데이터들 (String)
-                        user_data.setUser_phone_number(phone_number);
-                        user_data.setUser_birtyday(birthday);
-                        User.getInstance().setUser(user_data);
-                        intent.putExtra("access_key", User.getInstance().getData().getUser_access_key());
-                        intent.putExtra("name", User.getInstance().getData().getUser_name());
-                        intent.putExtra("id_email", User.getInstance().getData().getUser_email());
-                        intent.putExtra("gender", User.getInstance().getData().getUser_gender());
-                        intent.putExtra("phone_number", User.getInstance().getData().getUser_phone_number());
-                        intent.putExtra("birthday", User.getInstance().getData().getUser_birtyday());
-
-                        startActivity(intent);
-                        finish();
-                    } else {
-                        Toast.makeText(my_context, "가입에 실패하였습니다. 다시 시도해주세요.", Toast.LENGTH_LONG);
-                    }
-                }
+            @Override
+            public void afterTextChanged(Editable editable) {
             }
         });
-
     }
 
     @Override
     public void onBackPressed() {
-        Toast.makeText(getApplicationContext(),"가입을 완료해주세요",Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "가입을 완료해주세요", Toast.LENGTH_LONG).show();
     }
+
+    @OnClick(R.id.req_registerBtn)
+    public void onViewClicked() {
+        register();
+    }
+
+    public void register() {
+        String birthday = registerBirthdayYear.getText().toString() + registerBirthdayMonth.getText().toString() + registerBirthdayDay.getText().toString();
+        boolean isCorrectPhone = false;
+
+        if (registerPhone00.getText().toString().length() >= 3 && registerPhone01.getText().toString().length() >= 3 && registerPhone02.getText().toString().length() >= 4) {
+            isCorrectPhone = true;
+            String phone_number = registerPhone00.getText().toString() + "-" + registerPhone01.getText().toString() + "-" + registerPhone02.getText().toString();
+            String name = registerName.getText().toString();
+            if (!birthday.equals("") && isCorrectPhone && !name.equals("")) {
+                JSONObject send_data = new JSONObject();
+                try {
+                    user_data.setUser_name(name);
+                    send_data.put("access_key", user_data.getUser_access_key());
+                    send_data.put("gender", user_data.getUser_gender());
+                    send_data.put("birthday", birthday);
+                    send_data.put("phone_number", phone_number);
+                    send_data.put("name", user_data.getUser_name());
+                } catch (JSONException jsonex) {
+                    jsonex.printStackTrace();
+                }
+                Log.i("name data", user_data.getUser_name());
+                ReqHTTPJSONThread thread = new ReqHTTPJSONThread(Constants.REGISTER_MEMBER_URL, send_data);
+                thread.start();
+                try {
+                    thread.join();
+                } catch (InterruptedException interex) {
+                    interex.printStackTrace();
+                }
+                String result = thread.handler.getMsg();
+                JSONObject result_data = null;
+                boolean flag = false;
+
+                try {
+                    result_data = new JSONObject(result);
+                    if (result_data.getInt("code") == 1110) {
+                        flag = true;
+                    }
+                } catch (JSONException jsonex) {
+                    jsonex.printStackTrace();
+                }
+
+                if (flag) {
+                    Toast.makeText(my_context, "JUAN CROSS-FIT 가입을 환영합니다..", Toast.LENGTH_LONG);
+
+                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+
+                    // 메인 액티비티로 넘길 데이터들 (String)
+                    user_data.setUser_phone_number(phone_number);
+                    user_data.setUser_birtyday(birthday);
+                    User.getInstance().setUser(user_data);
+                    intent.putExtra("access_key", User.getInstance().getData().getUser_access_key());
+                    intent.putExtra("name", User.getInstance().getData().getUser_name());
+                    intent.putExtra("id_email", User.getInstance().getData().getUser_email());
+                    intent.putExtra("gender", User.getInstance().getData().getUser_gender());
+                    intent.putExtra("phone_number", User.getInstance().getData().getUser_phone_number());
+                    intent.putExtra("birthday", User.getInstance().getData().getUser_birtyday());
+
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(my_context, "가입에 실패하였습니다. 다시 시도해주세요.", Toast.LENGTH_LONG);
+                }
+            }
+        }
+    }
+
+
 }
