c9826c1a14b0e482ed57060008e5d5f300f62ccb
gyuhwan <gmarble94@gmail.com>
2017-10-01 21:58:00 +0900

    ScrollView에 이미지 추가

--- app/src/main/java/crossfit_juan/chk/com/crossfitjuan/Activity/AboutJuanActivity.java
+++ app/src/main/java/crossfit_juan/chk/com/crossfitjuan/Activity/AboutJuanActivity.java
@@ -1,17 +1,38 @@
 package crossfit_juan.chk.com.crossfitjuan.Activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import crossfit_juan.chk.com.crossfitjuan.R;
 
 public class AboutJuanActivity extends AppCompatActivity {
+
+    @BindView(R.id.aboutJuan_scroll)
+    ScrollView aboutJuanScroll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutjuan);
+        ButterKnife.bind(this);
+        setScrollViewContent();
+    }
+    void setScrollViewContent(){
+        ImageView imageView=new ImageView(this);
+        imageView.setAdjustViewBounds(true);
+        imageView.setImageResource(R.drawable.about_juan);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.gravity= Gravity.CENTER_HORIZONTAL;
+        imageView.setLayoutParams(lp);
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY); // 레이아웃 크기에 이미지를 맞춘다
 
+        aboutJuanScroll.addView(imageView);
     }
 
     @Override
