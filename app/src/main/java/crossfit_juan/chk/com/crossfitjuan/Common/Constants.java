a5e3bf4cab140de06b5609749881eb4afd8fe1c6
gyuhwan <gmarble94@gmail.com>
2017-10-11 00:42:19 +0900

    커뮤니티 및 접근 권한에 대한 상수 추가

--- app/src/main/java/crossfit_juan/chk/com/crossfitjuan/Common/Constants.java
+++ app/src/main/java/crossfit_juan/chk/com/crossfitjuan/Common/Constants.java
@@ -8,11 +8,37 @@ public class Constants {
 
     // 상수 변수, URL 등을 정리
 
+    // 네이버 로그인 상수 변수
+    public static final String OAUTH_CLIENT_ID = "0TrX_XsSZ5xhftBKYCzN";
+    public static final String OAUTH_CLIENT_SECRET= "GlE9Lv_n6W";
+    public static final String OAUTH_CLIENT_NAME="CrossfitJuan";
+
+    // 권한 접근 상수
+    public static final int COMMAND_CERTIFICATION_RESERVATION=0x01;
+    public static final int COMMAND_CERTIFICATION_MARKET=0x02;
+    public static final int CERTIFICATION_RESERVATION=0x02;
+    public static final int CERTIFICATION_MARKET=0x01;
+
     // 상수 변수
     public static final String Master_Socket_Address = "http://52.78.197.131:25478";
     public static final String DB_FILE_NAME="CrossFitJuan.db";
     public static final String CROSSFITJUAN_ADDRESS_GOOGLE="https://www.google.co.kr/maps/place/%EC%9D%B8%EC%B2%9C%EA%B4%91%EC%97%AD%EC%8B%9C+%EB%82%A8%EA%B5%AC+%EC%A3%BC%EC%95%88%EB%A1%9C+100";
     public static final int REQUEST_PERMISSION_ACCESS_STORAGE =0x80;
+
+    // 유저 정보 상수 변수
+    public static final int USER_INFO_INDEX_REGISTER_PERIOD=4;
+    public static final int USER_INFO_INDEX_LOGOUT=5;
+    public static final int USER_INFO_INDEX_SIGNOUT=6;
+
+    // 커뮤니티 주소
+    public static final String CROSSFITJUAN_NAVER_CAFE_URL="http://cafe.naver.com/cfja";
+    public static final String CROSSFITJUAN_FACEBOOK_URL="https://www.facebook.com/crossfit.juan";
+    public static final String CROSSFITJUAN_NAVER_CAFE_CALL_URL="navercafe://cafe?cafeUrl="+CROSSFITJUAN_NAVER_CAFE_URL+"&appId=crossfit_juan.chk.com.crossfitjuan";
+    public static final String CROSSFITJUAN_INSTAGRAM_URL="https://www.instagram.com/crossfit.juan/";
+
+    public static final String APP_ID="crossfit_juan.chk.com.crossfitjuan";
+    public static final String CAFE_URL="cfja";
+
 
     // URL
     public static final String REQ_LOGIN_URL = "http://52.78.197.131:25478/check_email";
@@ -31,6 +57,7 @@ public class Constants {
     public static final String REQ_MARKET_ITEM_PURCHASE="http://52.78.197.131:25478/add_item";
     public static final String REQ_MARKET_ITEM_PURCHASE_CANCEL="http://52.78.197.131:25478/sub_item";
 
+    public static final String REQ_SET_REMAIN_DAY="http://52.78.197.131:25478/set_remain_day";
     public static final String REQ_REGISTER_FIREBASE_TOKEN="http://52.78.197.131:25478/udt_device_token";
 
 
