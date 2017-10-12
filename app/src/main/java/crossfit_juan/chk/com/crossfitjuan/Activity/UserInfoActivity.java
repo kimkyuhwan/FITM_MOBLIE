b1d35618c26de34fef7e6b5fd89dd9395e7b6f2b
klight1994 <gmarble94@gmail.com>
2017-10-12 15:30:33 +0900

    휴회 기능 시 날짜 선택하도록 수정

--- app/src/main/java/crossfit_juan/chk/com/crossfitjuan/Activity/UserInfoActivity.java
+++ app/src/main/java/crossfit_juan/chk/com/crossfitjuan/Activity/UserInfoActivity.java
@@ -2,6 +2,7 @@ package crossfit_juan.chk.com.crossfitjuan.Activity;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -11,12 +12,19 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,6 +37,11 @@ import org.json.JSONObject;
 import java.io.File;
 import java.io.InputStream;
 import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -43,6 +56,7 @@ import crossfit_juan.chk.com.crossfitjuan.tool.ChatDBHelper;
 import crossfit_juan.chk.com.crossfitjuan.tool.CircleImageView;
 import crossfit_juan.chk.com.crossfitjuan.tool.UserInfoItemViewAdapter;
 
+import static crossfit_juan.chk.com.crossfitjuan.Common.Constants.CERTIFICATION_REST;
 import static crossfit_juan.chk.com.crossfitjuan.Common.Constants.DB_FILE_NAME;
 import static crossfit_juan.chk.com.crossfitjuan.Common.Constants.OAUTH_CLIENT_ID;
 import static crossfit_juan.chk.com.crossfitjuan.Common.Constants.OAUTH_CLIENT_NAME;
@@ -50,8 +64,12 @@ import static crossfit_juan.chk.com.crossfitjuan.Common.Constants.OAUTH_CLIENT_S
 import static crossfit_juan.chk.com.crossfitjuan.Common.Constants.PICK_FROM_ALBUM_ACTION;
 import static crossfit_juan.chk.com.crossfitjuan.Common.Constants.PROFILE_PATH;
 import static crossfit_juan.chk.com.crossfitjuan.Common.Constants.REQUEST_PERMISSION_ACCESS_STORAGE;
+import static crossfit_juan.chk.com.crossfitjuan.Common.Constants.REST_STATE_DONE;
+import static crossfit_juan.chk.com.crossfitjuan.Common.Constants.REST_STATE_NONE;
+import static crossfit_juan.chk.com.crossfitjuan.Common.Constants.REST_STATE_PENDING;
+import static crossfit_juan.chk.com.crossfitjuan.Common.Constants.REST_STATE_REMAIN;
 import static crossfit_juan.chk.com.crossfitjuan.Common.Constants.USER_INFO_INDEX_LOGOUT;
-import static crossfit_juan.chk.com.crossfitjuan.Common.Constants.USER_INFO_INDEX_REGISTER_PERIOD;
+import static crossfit_juan.chk.com.crossfitjuan.Common.Constants.USER_INFO_INDEX_REST_PERIOD;
 import static crossfit_juan.chk.com.crossfitjuan.Common.Constants.USER_INFO_INDEX_SIGNOUT;
 
 public class UserInfoActivity extends AppCompatActivity {
@@ -60,6 +78,8 @@ public class UserInfoActivity extends AppCompatActivity {
     private final String TAG = "DEBUG";
     Uri ImageCaptureUrl = null;
     Bitmap bm, resized;
+
+    Date StartDate, EndDate;
 
     @BindView(R.id.user_info_profile_image)
     CircleImageView userInfoProfileImage;
@@ -85,8 +105,6 @@ public class UserInfoActivity extends AppCompatActivity {
 
     public void initAdapter(){
         adapter=new UserInfoItemViewAdapter(this);
-        adapter.addItem(new UserInfoData("이메일",User.getInstance().getData().getUser_email(),"",false));
-        adapter.addItem(new UserInfoData("휴대폰 번호",User.getInstance().getData().getUser_phone_number(),"",false));
         int user_certfication=User.getInstance().getData().getCertification();
         String user_rank="JUAN Crossfit ";
         if(user_certfication==0){
@@ -98,21 +116,25 @@ public class UserInfoActivity extends AppCompatActivity {
         else if(user_certfication==2){
             user_rank+="정회원";
         }
-        adapter.addItem(new UserInfoData("회원등급",user_rank,"",false));
+        adapter.addItem(new UserInfoData("회원등급",user_rank,""));
         String locker_num="";
         int lock_num=User.getInstance().getData().getUser_locker_num();
         if(lock_num!=-1){
             locker_num=String.valueOf(lock_num);
             locker_num+=" ("+User.getInstance().getData().getUser_start_date()+"~"+User.getInstance().getData().getUser_start_date()+")";
         }
-        adapter.addItem(new UserInfoData("락커 번호",locker_num,"락커를 등록해 주세요",false));
+        adapter.addItem(new UserInfoData("락커 번호",locker_num,"락커를 등록해 주세요"));
         String period="";
         if(User.getInstance().getData().getUser_start_date()!=null && !User.getInstance().getData().getUser_start_date().equals("")){
             period=User.getInstance().getData().getUser_start_date()+"~"+User.getInstance().getData().getUser_finish_date();
         }
-        adapter.addItem(new UserInfoData("등록기간",period,"등록되지 않은 사용자 입니다",true));
-        adapter.addItem(new UserInfoData("로그아웃","","로그아웃 시 회원정보는 삭제되지 않습니다",false));
-        adapter.addItem(new UserInfoData("회원탈퇴","","회원 정보와 함께 기존의 대화내용이 모두 지워집니다",false));
+        adapter.addItem(new UserInfoData("등록기간",period,"등록되지 않은 사용자 입니다"));
+        String restStr="현재 잔여 일수는 "+User.getInstance().getData().getRemain_break_day()+"일 입니다";
+        adapter.addItem(new UserInfoData("휴회신청","",restStr));
+        adapter.addItem(new UserInfoData("이메일",User.getInstance().getData().getUser_email(),""));
+        adapter.addItem(new UserInfoData("휴대폰 번호",User.getInstance().getData().getUser_phone_number(),""));
+        adapter.addItem(new UserInfoData("로그아웃","","로그아웃 시 회원정보는 삭제되지 않습니다"));
+        adapter.addItem(new UserInfoData("회원탈퇴","","회원 정보와 함께 기존의 대화내용이 모두 지워집니다"));
         userInfoList.setAdapter(adapter);
     }
 
@@ -121,8 +143,18 @@ public class UserInfoActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int idx, long l) {
                 switch (idx){
-                    case USER_INFO_INDEX_REGISTER_PERIOD:
-                        Toast.makeText(getApplicationContext(),"휴회버튼 구현 예정",Toast.LENGTH_SHORT).show();
+                    case USER_INFO_INDEX_REST_PERIOD:
+                        if(User.getInstance().getData().getCertification()>=CERTIFICATION_REST) {
+                            if(User.getInstance().getData().getRest_state()==REST_STATE_NONE){
+                                Toast.makeText(getApplicationContext(),"잔여 일수가 부족합니다",Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                rest();
+                            }
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(),"휴회 기능은 정회원부터 가능합니다",Toast.LENGTH_SHORT).show();
+                        }
                         break;
                     case USER_INFO_INDEX_LOGOUT:
                         mOAuthLoginModule.logout(getApplicationContext());
@@ -136,6 +168,53 @@ public class UserInfoActivity extends AppCompatActivity {
         });
     }
 
+    void setRestState(){
+        if(User.getInstance().getData().getRemain_break_day()==0){
+            User.getInstance().getData().setRest_state(REST_STATE_NONE);
+        }
+        else{
+            int state=REST_STATE_REMAIN;
+            try {
+                state=checkRestState();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            User.getInstance().getData().setRest_state(state);
+        }
+        Log.d("DEBUGYU","State : "+User.getInstance().getData().getRest_state());
+    }
+
+    int checkRestState() throws JSONException {
+        JSONObject send_data = new JSONObject();
+        try{
+            send_data.put("access_key", User.getInstance().getData().getUser_access_key());
+        }catch (JSONException jsonex){
+            jsonex.printStackTrace();
+        }
+        ReqHTTPJSONThread thread = new ReqHTTPJSONThread(Constants.REQ_GET_REMAIN_DAY, send_data);
+        thread.start();
+        try{
+            thread.join();
+        }catch (InterruptedException interex){
+            interex.printStackTrace();
+        }
+        String result = thread.handler.getMsg();
+        JSONObject result_data = new JSONObject(result);
+        int result_code = result_data.getInt("code");
+        // JSONObject response=result_data.getJSONObject(  "response");
+        if(result_code==9999){
+            JSONObject resultData=result_data.getJSONObject("result");
+            int state=resultData.getInt("state");
+            if(state==0){
+                return REST_STATE_PENDING;
+            }
+            else{
+                return REST_STATE_DONE;
+            }
+
+        }
+        return REST_STATE_REMAIN;
+    }
 
     public void TakeImagetoAlbum() {
         Intent intent = new Intent(Intent.ACTION_PICK);
@@ -193,6 +272,106 @@ public class UserInfoActivity extends AppCompatActivity {
         });
         ImageSetThread.start();
     }
+
+    void rest(){
+        String EndDate_String=User.getInstance().getData().getUser_finish_date();
+        try {
+            EndDate=new SimpleDateFormat("yyyyMMdd").parse(EndDate_String);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        final Dialog startdR = new Dialog(this);
+        startdR.setContentView(R.layout.register_rest_dialog);
+        TextView start_dRTitle=(TextView)startdR.findViewById(R.id.rest_dialog_title);
+        final CalendarView start_dRCalander=(CalendarView)startdR.findViewById(R.id.rest_dialog_calander);
+        Button start_dRRegisterBtn=(Button)startdR.findViewById(R.id.rest_dialog_comment_register_Btn);
+        Button start_dRCancelBtn=(Button)startdR.findViewById(R.id.rest_dialog_comment_cancel_Btn);
+        start_dRCalander.setMinDate(start_dRCalander.getDate());
+        start_dRCalander.setMaxDate(EndDate.getTime());
+        start_dRCalander.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                StartDate=(new GregorianCalendar(year,month,dayOfMonth)).getTime();
+                GregorianCalendar gregorianCalendar=(new GregorianCalendar(year,month,dayOfMonth));
+                gregorianCalendar.add(Calendar.DATE, User.getInstance().getData().getRemain_break_day()-1);
+                EndDate=gregorianCalendar.getTime();
+            }
+        });
+        start_dRRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                long start_date=StartDate.getTime();
+                long end_date=EndDate.getTime();
+                Date date=new Date(start_date);
+                Log.d("DEBUGYU",date.toString());
+                final Dialog enddR = new Dialog(UserInfoActivity.this);
+                enddR.setContentView(R.layout.register_rest_dialog);
+                TextView end_dRTitle=(TextView)enddR.findViewById(R.id.rest_dialog_title);
+                final CalendarView end_dRCalander=(CalendarView)enddR.findViewById(R.id.rest_dialog_calander);
+                Button end_dRRegisterBtn=(Button)enddR.findViewById(R.id.rest_dialog_comment_register_Btn);
+                Button end_dRCancelBtn=(Button)enddR.findViewById(R.id.rest_dialog_comment_cancel_Btn);
+                end_dRCalander.setMinDate(start_date);
+                end_dRCalander.setDate(start_date);
+                end_dRCalander.setMaxDate(end_date);
+                end_dRTitle.setText("휴회 종료 일자를 선택해주세요");
+                end_dRCalander.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+                    @Override
+                    public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                        EndDate=(new GregorianCalendar(year,month,dayOfMonth)).getTime();
+                    }
+                });
+                end_dRRegisterBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.d("DEBUGYU",StartDate.toString()+"~"+EndDate.toString());
+                        enddR.dismiss();
+                    }
+                });
+                end_dRCancelBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        enddR.dismiss();
+                    }
+                });
+                enddR.show();
+                startdR.dismiss();
+            }
+        });
+        start_dRCancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startdR.dismiss();
+            }
+        });
+        startdR.show();
+    }
+    void RegisterRest(String body) throws JSONException {
+        JSONObject send_data = new JSONObject();
+        try{
+            send_data.put("access_key", User.getInstance().getData().getUser_access_key());
+            send_data.put("comments",body);
+        }catch (JSONException jsonex){
+            jsonex.printStackTrace();
+        }
+        ReqHTTPJSONThread thread = new ReqHTTPJSONThread(Constants.REQ_SET_REMAIN_DAY, send_data);
+        thread.start();
+        try{
+            thread.join();
+        }catch (InterruptedException interex){
+            interex.printStackTrace();
+        }
+        String result = thread.handler.getMsg();
+        JSONObject result_data = new JSONObject(result);
+        int result_code = result_data.getInt("code");
+        // JSONObject response=result_data.getJSONObject(  "response");
+        if(result_code==8888){
+            Log.d("DEBUGYU","신청되었습니다");
+        }
+        else if(result_code==5800){
+            Log.d("DEBUGYU","신청에 실패했습니다");
+        }
+    }
+
 
     void LogoutUserCheckDialog() {
         AlertDialog.Builder alt_bld = new AlertDialog.Builder(this);
@@ -315,6 +494,9 @@ public class UserInfoActivity extends AppCompatActivity {
     public void onResume() {
         super.onResume();
         SetProfileImage();
+        User.setHereActivityContext(this);
+        User.setHereActivity("UserInfo");
+        setRestState();
     }
 
 
