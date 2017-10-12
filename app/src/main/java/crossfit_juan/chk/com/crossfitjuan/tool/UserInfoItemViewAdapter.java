9b5b95cb5080cb304cea8e0ece056afcdd77ddd0
klight1994 <gmarble94@gmail.com>
2017-10-12 15:24:39 +0900

    이미지 버튼 삭제

--- app/src/main/java/crossfit_juan/chk/com/crossfitjuan/tool/UserInfoItemViewAdapter.java
+++ app/src/main/java/crossfit_juan/chk/com/crossfitjuan/tool/UserInfoItemViewAdapter.java
@@ -64,7 +64,6 @@ public class UserInfoItemViewAdapter extends BaseAdapter {
         if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = inflater.inflate(R.layout.listview_userinfo_item, parent, false);
-            ImageButton itemImg=(ImageButton)convertView.findViewById(R.id.user_info_item_imageBtn);
             TextView titleText=(TextView)convertView.findViewById(R.id.user_info_item_title);
             TextView bodyText=(TextView)convertView.findViewById(R.id.user_info_item_body);
             // 화면에 표시될 View(Layout이 inflate된)으로부터 위젯에 대한 참조 획득
@@ -73,19 +72,10 @@ public class UserInfoItemViewAdapter extends BaseAdapter {
 
             // Data Set(listViewItemList)에서 position에 위치한 데이터 참조 획득
             UserInfoData pp = listViewItemList.get(position);
-            itemImg.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    rest();
-                }
-            });
             // 아이템 내 각 위젯에 데이터 반영
             titleText.setText(pp.getTitle());
             bodyText.setText(pp.getBody());
             bodyText.setHint(pp.getHint());
-            if(pp.isBtnVisible()){
-                itemImg.setVisibility(View.VISIBLE);
-            }
             //String price=setPriceString(pp.getPrice());
         }
 
@@ -93,91 +83,6 @@ public class UserInfoItemViewAdapter extends BaseAdapter {
         return convertView;
     }
 
-    void rest(){
-        final Dialog dR = new Dialog(context);
-        dR.setContentView(R.layout.register_rest_dialog);
-        TextView dRTitle=(TextView)dR.findViewById(R.id.rest_dialog_title);
-        final EditText dRcomment=(EditText)dR.findViewById(R.id.rest_dialog_comment);
-        final ImageButton dRcommentClearBtn=(ImageButton)dR.findViewById(R.id.rest_dialog_comment_clear_Btn);
-        Button dRRegisterBtn=(Button)dR.findViewById(R.id.rest_dialog_comment_register_Btn);
-        Button dRCancelBtn=(Button)dR.findViewById(R.id.rest_dialog_comment_cancel_Btn);
-        dRcomment.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // do nothing
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(s.length()==0){
-                    dRcommentClearBtn.setVisibility(View.INVISIBLE);
-                }
-                else{
-                    dRcommentClearBtn.setVisibility(View.VISIBLE);
-                }
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                // do nothing
-            }
-        });
-        String title=dRTitle.getText().toString();
-        title+="\n(남은 휴회 일수는 "+User.getInstance().getData().getRemain_break_day()+"일 입니다)";
-        dRTitle.setText(title);
-        dRcommentClearBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dRcomment.setText("");
-            }
-        });
-        dRRegisterBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String tx=dRcomment.getText().toString();
-                try {
-                    RegisterRest(tx);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                Log.d("DEBUGYU",tx);
-                dR.dismiss();
-            }
-        });
-        dRCancelBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dR.dismiss();
-            }
-        });
-        dR.show();
-    }
-    void RegisterRest(String body) throws JSONException {
-        JSONObject send_data = new JSONObject();
-        try{
-            send_data.put("access_key", User.getInstance().getData().getUser_access_key());
-            send_data.put("comments",body);
-        }catch (JSONException jsonex){
-            jsonex.printStackTrace();
-        }
-        ReqHTTPJSONThread thread = new ReqHTTPJSONThread(Constants.REQ_SET_REMAIN_DAY, send_data);
-        thread.start();
-        try{
-            thread.join();
-        }catch (InterruptedException interex){
-            interex.printStackTrace();
-        }
-        String result = thread.handler.getMsg();
-        JSONObject result_data = new JSONObject(result);
-        int result_code = result_data.getInt("code");
-       // JSONObject response=result_data.getJSONObject(  "response");
-        if(result_code==8888){
-            Log.d("DEBUGYU","신청되었습니다");
-        }
-        else if(result_code==5800){
-            Log.d("DEBUGYU","신청에 실패했습니다");
-        }
-    }
 
 
     public UserInfoData getMarketItem(int position){
