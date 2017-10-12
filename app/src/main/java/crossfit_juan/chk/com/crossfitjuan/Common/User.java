2c56282ae18cf1d27395b8736c81ffd6f9cfb8d6
klight1994 <gmarble94@gmail.com>
2017-10-12 15:26:35 +0900

    푸시 알림 시 사용할 Context, ActivityName 변수 추가

--- app/src/main/java/crossfit_juan/chk/com/crossfitjuan/Common/User.java
+++ app/src/main/java/crossfit_juan/chk/com/crossfitjuan/Common/User.java
@@ -1,5 +1,7 @@
 package crossfit_juan.chk.com.crossfitjuan.Common;
 
+import android.content.Context;
+
 import crossfit_juan.chk.com.crossfitjuan.DataModel.UserData;
 
 /**
@@ -10,16 +12,33 @@ public class User {
     private static User Me=new User();
 
     private static UserData data;
-
+    private static Context hereActivityContext;
+    private static String hereActivity;
     private User(){}
     public static User getInstance(){
         return Me;
     }
     public void setUser(UserData data){
-        this.data=data;
+        User.data =data;
     }
 
     public UserData getData() {
         return data;
+    }
+
+    public static Context getHereActivityContext() {
+        return hereActivityContext;
+    }
+
+    public static void setHereActivityContext(Context hereActivityContext) {
+        User.hereActivityContext = hereActivityContext;
+    }
+
+    public static String getHereActivity() {
+        return hereActivity;
+    }
+
+    public static void setHereActivity(String hereActivity) {
+        User.hereActivity = hereActivity;
     }
 }
