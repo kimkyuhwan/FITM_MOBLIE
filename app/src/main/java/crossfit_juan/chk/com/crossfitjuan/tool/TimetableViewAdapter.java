c86dc440582b2951d116b87a21e7b42a8f30a633
gyuhwan <gmarble94@gmail.com>
2017-11-02 21:59:48 +0900

    시간표 아이템 어댑터 기능 수정 (예약 취소시 댓글 숫자 줄임)

--- app/src/main/java/crossfit_juan/chk/com/crossfitjuan/tool/TimetableViewAdapter.java
+++ app/src/main/java/crossfit_juan/chk/com/crossfitjuan/tool/TimetableViewAdapter.java
@@ -21,6 +21,8 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
+import crossfit_juan.chk.com.crossfitjuan.Activity.MainActivity;
+import crossfit_juan.chk.com.crossfitjuan.Activity.ReservationActivity;
 import crossfit_juan.chk.com.crossfitjuan.Common.Constants;
 import crossfit_juan.chk.com.crossfitjuan.Common.User;
 import crossfit_juan.chk.com.crossfitjuan.DataModel.Classinfo;
@@ -42,6 +44,7 @@ public class TimetableViewAdapter extends BaseAdapter {
     Context context;
     String Date;
     Participant me;
+    boolean isReserved=true;
     // ListViewAdapter의 생성자
     public TimetableViewAdapter(Context context,String Date) {
         this.context=context;
@@ -79,20 +82,14 @@ public class TimetableViewAdapter extends BaseAdapter {
 
         // 아이템 내 각 위젯에 데이터 반영
         timeTextView.setText(pp.getStart_time()+"~"+pp.getFinish_time());
-        timeTextView.setOnClickListener(new View.OnClickListener() {
+        /*timeTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ViewParticipantsList(pos);
             }
         });
-        participantsTextView.setText(pp.getParticipants_size()+" / "+pp.getMax_participant());
+        */participantsTextView.setText(pp.getParticipants_size()+" / "+pp.getMax_participant());
 
-        participantsTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ViewParticipantsList(pos);
-            }
-        });
         if(pp.getMax_participant() == pp.getParticipants().size()){
             if(pp.getSelected_state()!=RESERVATION_SELECT_STATE_SELECTED)
             pp.setSelected_state(RESERVATION_SELECT_STATE_NOT_SELECTED);
@@ -100,15 +97,7 @@ public class TimetableViewAdapter extends BaseAdapter {
         layout.setBackgroundColor(Color.argb(0xff,0xff,0xff,0xff));
         switch (pp.getSelected_state()){
             case RESERVATION_SELECT_STATE_NONE:
-                imageButton.setVisibility(View.VISIBLE);
-                imageButton.setBackgroundResource(R.drawable.right_arrow);
-                imageButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Log.d("DEBUGYU","RESERVATION_SELECT_STATE_NONE");
-                        RegisterReservation(pos);
-                    }
-                });
+                imageButton.setVisibility(View.INVISIBLE);
                 break;
             case RESERVATION_SELECT_STATE_SELECTED:
                 layout.setBackgroundColor(Color.argb(0x66,0x44,0x88,0xff));
@@ -117,8 +106,10 @@ public class TimetableViewAdapter extends BaseAdapter {
                 imageButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Log.d("DEBUGYU","RESERVATION_SELECT_STATE_SELECTED");
-                        CancelReservation(pos);
+                        if(isReserved) {
+                            Log.d("DEBUGYU", "RESERVATION_SELECT_STATE_SELECTED");
+                            CancelReservation(pos);
+                        }
                     }
                 });
                 break;
@@ -130,81 +121,25 @@ public class TimetableViewAdapter extends BaseAdapter {
         return convertView;
     }
 
-    public void ViewParticipantsList(int idx){
-        Log.d("DEBUGYU","VIEW PARTICIPATNS");
-        final Dialog dR = new Dialog(context);
-        dR.setContentView(R.layout.view_participants_dialog);
-        TextView dRtime = (TextView) dR.findViewById(R.id.participant_dialog_time);
-        ListView dRlist = (ListView) dR.findViewById(R.id.participant_dialog_list);
-        Button dRCancelBtn = (Button) dR.findViewById(R.id.participant_dialog_cancel_Btn);
-        dRtime.setText(listViewItemList.get(idx).getStart_time()+"~"+listViewItemList.get(idx).getFinish_time());
-        ParticipantsViewAdapter adapter=new ParticipantsViewAdapter();
-        for(int i=0;i<listViewItemList.get(idx).getParticipants().size();i++){
-            adapter.addItem(listViewItemList.get(idx).getParticipants().get(i));
-            Log.d("DEBUGYU","VIEW PARTICIPATNS"+i+" : "+listViewItemList.get(idx).getParticipants().get(i).toString());
-        }
-        dRlist.setAdapter(adapter);
-        dRCancelBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dR.dismiss();
-            }
-        });
-        dR.show();
+    public Classinfo getClassInfo(int idx){
+        return listViewItemList.get(idx);
+    }
+    // 지정한 위치(position)에 있는 데이터와 관계된 아이템(row)의 ID를 리턴. : 필수 구현
+    @Override
+    public long getItemId(int position) {
+        return position ;
     }
 
-    public void RegisterReservation(int idx) {
-        JSONObject send_data = new JSONObject();
-        try {
-            send_data.put("access_key", User.getInstance().getData().getUser_access_key());
-            send_data.put("date", Date);
-            send_data.put("class_num", idx + 1);
-            send_data.put("name", User.getInstance().getData().getUser_name());
-            send_data.put("id_email",User.getInstance().getData().getUser_email());
-        } catch (JSONException jsonex) {
-            jsonex.printStackTrace();
-        }
-        Log.d("DEBUGYU","SEND DATA : "+send_data.toString());
-        ReqHTTPJSONThread thread = new ReqHTTPJSONThread(Constants.REQ_REGISTER_RESERVATION, send_data);
-        thread.start();
-        try {
-            thread.join();
-        } catch (InterruptedException interex) {
-            interex.printStackTrace();
-        }
-        String result = thread.handler.getMsg();
-        JSONObject result_data = null;
-        int result_code = 0;
-        try {
-            result_data = new JSONObject(result);
-            result_code = result_data.getInt("code");
-            JSONObject response = result_data.getJSONObject("response");
-            Log.d("DEBUGYU", String.valueOf(result_code));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        if (result_code == 1350) {
-            Toast.makeText(context, "등록되었습니다", Toast.LENGTH_LONG).show();
-            for(int i=0;i<listViewItemList.size();i++){
-                if(i!=idx)
-                    listViewItemList.get(i).setSelected_state(RESERVATION_SELECT_STATE_NOT_SELECTED);
-                else{
-                    listViewItemList.get(i).setSelected_state(RESERVATION_SELECT_STATE_SELECTED);
-                    listViewItemList.get(idx).getParticipants().add(me);
-                    listViewItemList.get(i).addParticipants();
-                }
-            }
-            notifyDataSetChanged();
-        } else if (result_code == 1270) {
-            Toast.makeText(context, "잘못된 요청입니다", Toast.LENGTH_LONG).show();
-        }
-        else{
-            Log.d("DEBUGYU","CODE "+result_code);
-        }
+    // 지정한 위치(position)에 있는 데이터 리턴 : 필수 구현
+    @Override
+    public Object getItem(int position) {
+        return listViewItemList.get(position) ;
     }
 
-
-
+    // 아이템 데이터 추가를 위한 함수. 개발자가 원하는대로 작성 가능.
+    public void addItem(Classinfo pp) {
+        listViewItemList.add(pp);
+    }
     public void CancelReservation(int idx) {
         JSONObject send_data = new JSONObject();
         try {
@@ -231,9 +166,12 @@ public class TimetableViewAdapter extends BaseAdapter {
             e.printStackTrace();
         }
         if (result_code == 1270) {
+            isReserved=false;
             Toast.makeText(context, "취소되었습니다", Toast.LENGTH_LONG).show();
             //scheduleRegisterBtn.setText("등록");
             //selectScheduleBtn.setClickable(!isReserved);
+
+            ((ReservationActivity)context).commentText.setText("댓글("+--((ReservationActivity)context).count+")");
             for(int i=0;i<listViewItemList.size();i++){
                 if(listViewItemList.get(i).getMax_participant()==listViewItemList.get(i).getParticipants().size())
                     listViewItemList.get(i).setSelected_state(RESERVATION_SELECT_STATE_NOT_SELECTED);
@@ -256,25 +194,11 @@ public class TimetableViewAdapter extends BaseAdapter {
         }
     }
 
-    public Classinfo getClassInfo(int idx){
-        return listViewItemList.get(idx);
-    }
-    // 지정한 위치(position)에 있는 데이터와 관계된 아이템(row)의 ID를 리턴. : 필수 구현
-    @Override
-    public long getItemId(int position) {
-        return position ;
+    public boolean isReserved() {
+        return isReserved;
     }
 
-    // 지정한 위치(position)에 있는 데이터 리턴 : 필수 구현
-    @Override
-    public Object getItem(int position) {
-        return listViewItemList.get(position) ;
+    public void setReserved(boolean reserved) {
+        isReserved = reserved;
     }
-
-    // 아이템 데이터 추가를 위한 함수. 개발자가 원하는대로 작성 가능.
-    public void addItem(Classinfo pp) {
-        listViewItemList.add(pp);
-    }
-
-
 }
