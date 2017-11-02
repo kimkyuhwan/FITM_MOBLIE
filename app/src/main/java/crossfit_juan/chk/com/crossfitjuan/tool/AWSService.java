acf97b80f40af98e9233f9cd63cb400ea1e5ec8d
gyuhwan <gmarble94@gmail.com>
2017-11-02 21:58:11 +0900

    랭킹 등록 기능 및 이미지 사이즈 줄이기 기능 추가

--- app/src/main/java/crossfit_juan/chk/com/crossfitjuan/tool/AWSService.java
+++ app/src/main/java/crossfit_juan/chk/com/crossfitjuan/tool/AWSService.java
@@ -2,8 +2,10 @@ package crossfit_juan.chk.com.crossfitjuan.tool;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.os.Message;
+import android.util.Log;
 
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSCredentials;
@@ -22,8 +24,12 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import crossfit_juan.chk.com.crossfitjuan.Common.User;
+
+import static crossfit_juan.chk.com.crossfitjuan.Common.Constants.PROFILE_PATH;
 
 /**
  * Created by gyuhwan on 2017. 9. 26..
@@ -67,6 +73,16 @@ public class AWSService {
         }
         InstanceReload();
     }
+    public void upload_rank(File file,String date){
+        uploadRankThread A=new uploadRankThread(file,date);
+        A.start();
+        try {
+            A.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        InstanceReload();
+    }
 
     public void DeleteProfile(){
         deleteFileThread A=new deleteFileThread();
@@ -95,11 +111,47 @@ public class AWSService {
     public void uploadFile(File file) {
         if (amazonS3 != null) {
             try {
+                Bitmap smallImg=BitmapFactory.decodeFile(file.getAbsolutePath());
+                File file2=createThumbnail(smallImg,file.getParent(),User.getInstance().getData().getUser_email()+".png",240);
+                Log.w("DEBUGYU",file2.getAbsolutePath());
                 PutObjectRequest putObjectRequest =
-                        new PutObjectRequest(BUCKET_NAME /*sub directory*/, User.getInstance().getData().getUser_email()+".png", file);
+                        new PutObjectRequest(BUCKET_NAME /*sub directory*/, User.getInstance().getData().getUser_email()+".png", file2);
                 putObjectRequest.setCannedAcl(CannedAccessControlList.PublicRead); // file permission
                 amazonS3.putObject(putObjectRequest); // upload file
+                file2.delete();
+            } catch (AmazonServiceException ase) {
+                ase.printStackTrace();
+            } finally {
+                amazonS3 = null;
+            }
+        }
+    }
 
+
+    private class uploadRankThread extends Thread{
+
+        private File file;
+        private String date;
+        uploadRankThread(File file,String date){
+            this.file=file;
+            this.date=date;
+        }
+        public void run() {
+            uploadRank(file,date);
+        }
+
+    }
+
+    public void uploadRank(File file,String date) {
+        if (amazonS3 != null) {
+            try {
+                Bitmap smallImg=BitmapFactory.decodeFile(file.getAbsolutePath());
+                File file2=createThumbnail(smallImg,file.getParent(),User.getInstance().getData().getUser_email()+".png",480);
+                PutObjectRequest putObjectRequest =
+                        new PutObjectRequest(BUCKET_NAME /*sub directory*/, date+"_ranking.png", file2);
+                putObjectRequest.setCannedAcl(CannedAccessControlList.PublicRead); // file permission
+                amazonS3.putObject(putObjectRequest); // upload file
+                file2.delete();
             } catch (AmazonServiceException ase) {
                 ase.printStackTrace();
             } finally {
@@ -125,6 +177,49 @@ public class AWSService {
                 amazonS3 = null;
             }
         }
+    }
+    public static File createThumbnail(Bitmap bitmap, String strFilePath,
+                                       String filename,int Size) {
+
+        File file = new File(strFilePath);
+
+        // If no folders
+        if (!file.exists()) {
+            file.mkdirs();
+            // Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
+        }
+        File fileCacheItem = new File(strFilePath+ "/" + filename);
+        OutputStream out = null;
+
+        try {
+
+            int height=bitmap.getHeight();
+            int width=bitmap.getWidth();
+
+
+            fileCacheItem.createNewFile();
+            out = new FileOutputStream(fileCacheItem);
+            bitmap = Bitmap.createScaledBitmap(bitmap, Size, height/(width/Size), true);
+
+            if(width<height){
+                Matrix matrix=new Matrix();
+                matrix.postRotate(270);
+                Bitmap rotateBitmap=Bitmap.createBitmap(bitmap,0,0,bitmap.getWidth(),bitmap.getHeight(),matrix,true);
+                rotateBitmap.compress(Bitmap.CompressFormat.JPEG,100,out);
+            }
+            else{
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                out.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return fileCacheItem;
     }
 
 
